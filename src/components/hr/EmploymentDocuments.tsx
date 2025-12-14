b8a964d404c97c3b748c07760697bc2e81154d19
Prashant <prashant@Deeptis-MacBook-Air.local>
2025-12-14 16:33:39 +0000

    IFSC fix

--- src/components/hr/EmploymentDocuments.tsx
+++ src/components/hr/EmploymentDocuments.tsx
@@ -2418,8 +2418,8 @@ const EmploymentDocuments: React.FC<EmploymentDocumentsProps> = ({ onBackToDashb
                 </label>
                 <input
                   type="text"
-                  value={employeeForm.ifsc_code || ''}
-                  onChange={(e) => setEmployeeForm({ ...employeeForm, ifsc_code: e.target.value })}
+                  value={employeeForm.bank_ifsc_code || ''}
+                  onChange={(e) => setEmployeeForm({ ...employeeForm, bank_ifsc_code: e.target.value })}
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
                 />
               </div>
